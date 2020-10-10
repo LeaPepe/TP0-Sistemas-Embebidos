df10e313380f706e7d3f8ab63510f0027373bc34
LeaPepe <leapalazzo@gmail.com>
2020-10-09 22:27:23 -0300

    P1) implementacion shape_t

--- src/image.c
+++ src/image.c
@@ -6,20 +6,24 @@
 #include "image.h"
 
 
-
+// constructor
 bool image_ctor(image_t *me, uint32_t n_rows, uint32_t n_cols){
 
 	me->n_rows = n_rows;
 	me->n_cols = n_cols;
 
+	// alloc memory for cols (pointers)
 	if ( !(me->data = (img_data_t **)malloc(sizeof(img_data_t *)*n_cols)) ){
 		return false;
 	}
 
+	// alloc memory for rows (bits)
 	for(uint32_t i=0; i<n_cols; i++){
 		if ( !(me->data[i] = (img_data_t *)malloc(sizeof(img_data_t)*n_rows)) ){
 			return false;
 		}
+		
+		// zero initialize
 		for(uint32_t j=0; j<n_rows; j++){
 			image_write(me, i, j, LOW);
 		}
@@ -28,29 +32,33 @@ bool image_ctor(image_t *me, uint32_t n_rows, uint32_t n_cols){
 	return true;
 }
 
-
+// destructor
 void image_dtor(image_t *me){
 
+	// why?
 	uint32_t n_cols = me->n_cols;
 	
+	// free data cols
 	for(uint32_t i=0; i<n_cols; i++){
 		free(me->data[i]);
 	}
 
+	// free row pointers
 	free(me->data);
 }
 
-
+// value write in row i col j
 void image_write(image_t *me, uint32_t i, uint32_t j, img_data_t value){
 	me->data[i][j] = value;
 }	
 
-
+// value read in row i col j
 img_data_t image_read(image_t *me, uint32_t i, uint32_t j){
 	return me->data[i][j];
 }
 
 
+// translate image to a file
 void image_to_file(image_t *me, const char *filename){
 
 	uint32_t n_cols, n_rows;
--- src/shape.c
+++ src/shape.c
@@ -1,28 +1,71 @@
-
+#include <stdlib.h>
 #include "shape.h"
 #include "image.h"
 
 
 bool shape_ctor(shape_t *me, coordinates_array_t *array, uint32_t position_x, uint32_t position_y){
-	// Completar
+	
+	// position
+	me->position.x = position_x;
+	me->position.y = position_y;
+	
+	
+	// alloc mem for coordinates array (maybe proper constructor?)
+	if ( !(me->array.coordinates = (coordinate_t*)malloc(sizeof(coordinate_t)*array->n_array)) ){
+		return false;
+	}
+	me->array.n_array = array->n_array;
+	
+	// copy array
+	for(uint32_t i=0; i<me->array.n_array; i++){
+		me->array.coordinates[i].x = array->coordinates[i].x;
+		me->array.coordinates[i].y = array->coordinates[i].y;
+	}
+	return true;
 }
 
+
 void shape_dtor(shape_t *me){
-	// Completar
+	// free the only array
+	free(me->array.coordinates);
 }
 
 bool shape_move(shape_t *me, uint32_t dx, uint32_t dy){
-	// Completar
+	
+	//make new array for moving 
+	coordinate_t *moved_coordinates;
+	if ( !(moved_coordinates = (coordinate_t*)malloc(sizeof(coordinate_t)*me->array.n_array)) ){
+		return false;
+	}
+	
+	// for each coordinate, move it
+	for(uint32_t i=0; i<me->array.n_array; i++){
+		// move coordinate, only positive so no troubles.
+		moved_coordinates[i].x = me->array.coordinates[i].x + dx;
+		moved_coordinates[i].y = me->array.coordinates[i].y + dy;
+	}
+	
+	//move position
+	me->position.x += dx;
+	me->position.y += dy;
+	
+	//destroy and replace
+	free(me->array.coordinates);
+	me->array.coordinates = moved_coordinates;
+	return true;
 }
 
 bool shape_rotate(shape_t *me, float angle){
 	// Completar
+	return true;
 }
 
 float shape_distance_from(shape_t *me, shape_t *shape){
 	// Completar
+	return 0;
 }
 
 bool shape_plot(shape_t *me, image_t *image){
 	// Completar
+	return true;
 }
